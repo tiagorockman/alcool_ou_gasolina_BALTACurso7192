43c824838757e28032c1749137ff0012d3d77c50
tiagorockman <tiagorockman@gmail.com>
2021-10-04 21:33:19 -0300

    Commit aula 10

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:alcool_ou_gasolina/widgets/input.widget.dart';
 import 'package:alcool_ou_gasolina/widgets/loading-button.widget.dart';
 import 'package:alcool_ou_gasolina/widgets/logo.widget.dart';
+import 'package:alcool_ou_gasolina/widgets/success.widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_masked_text/flutter_masked_text.dart';
 
@@ -33,37 +34,7 @@ class HomePage extends StatelessWidget {
       body: ListView(
         children: <Widget>[
           Logo(),
-          Container(
-            margin: EdgeInsets.all(30),
-            decoration: BoxDecoration(
-              color: Colors.white.withOpacity(0.8),
-              borderRadius: BorderRadius.circular(25),
-            ),
-            child: Column(
-              children: <Widget>[
-                SizedBox(
-                  height: 50,
-                ),
-                Text(
-                  "Compensa utilizar ácool",
-                  style: TextStyle(
-                    color: Theme.of(context).primaryColor,
-                    fontSize: 40,
-                    fontFamily: "Big Shoulders Display",
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                LoadingButton(
-                    busy: false,
-                    func: () {},
-                    invert: true,
-                    text: "CALCULAR NOVAMENTE"),
-              ],
-            ),
-          ),
+          Success(result: "Compensa utilizar X", reset: () {}),
           Input(
             ctrl: _gasController,
             label: "Gasolina",
